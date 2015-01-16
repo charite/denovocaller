b0d5f9d4295fe7566fcffecfc72b10387574202b
Marten Jaeger <marten.jaeger@charite.de>
2015-01-16 13:36:04 +0100

    first init

---
+++ src/main/java/de/charite/compbio/DVCoptions.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package de.charite.compbio;
+
+/**
+ * This class stores all provided options for the Denovo Variant Caller
+ *
+ * @author Marten Jäger <marten.jaeger@charite.de>
+ *
+ */
+public class DVCoptions {
+
+}
